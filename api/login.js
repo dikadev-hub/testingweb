c4fa0344dd307f3479a155cc4b04fb6ee34a9b54
dikadev-hub <dikabjir8@gmail.com>
2026-05-04 21:05:33 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
         });
     } catch (e) {
         return res.status(400).json({ 
-            message: "yah login gagal: " + e.message 
+            message: "error: " + e.message
         });
     }
 }
