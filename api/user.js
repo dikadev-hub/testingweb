6369fd68e40bedc3cdbcb2a66d2f3dffd7835e08
dikadev-hub <dikabjir8@gmail.com>
2026-05-04 20:56:54 +0700

    Create user.js

---
+++ api/user.js
@@ -0,0 +1,26 @@
+const Parse = require('parse/node');
+
+Parse.initialize(
+    "Y0f6hqTsxzNapkFRzIKO6b9pGENY8ewx3HMZu72k", 
+    "Y4R6Q4AwyZBfGppAeaXDfvfW8MNvIwhdHEJ7KoIc",
+    "QKhrsQmDyp2sSEosdc78N8AbJxzbYZFzEyiWc1nl"
+);
+Parse.serverURL = 'https://parseapi.back4app.com/';
+
+export default async function handler(req, res) {
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({ message: 'kamu belum login kak' });
+    }
+    try {
+        const user = await Parse.User.become(token);
+        return res.status(200).json({
+            success: true,
+            fullname: user.get('fullname'),
+            email: user.get('email'),
+            apiKey: user.get('apiKey') || 'key belum terbuat'
+        });
+    } catch (e) {
+        return res.status(401).json({ message: 'sesi habis' });
+    }
+}
